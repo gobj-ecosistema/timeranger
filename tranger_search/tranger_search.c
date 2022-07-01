819016169b29987026de9a55d9c3c0a8c7f413d9
ginsmar <ginsmar@gmail.com>
2022-07-01 19:07:53 +0200

    tranger_list: Examples TIME:\n  1.seconds (minutes,hours,days,weeks,months,years)

--- tranger_search/tranger_search.c
+++ tranger_search/tranger_search.c
@@ -741,41 +741,37 @@ int main(int argc, char *argv[])
 
     if(arguments.from_t) {
         timestamp_t timestamp;
-        int offset;
         if(all_numbers(arguments.from_t)) {
             timestamp = atoll(arguments.from_t);
         } else {
-            parse_date_basic(arguments.from_t, &timestamp, &offset);
+            timestamp = approxidate(arguments.from_t);
         }
         json_object_set_new(match_cond, "from_t", json_integer(timestamp));
     }
     if(arguments.to_t) {
         timestamp_t timestamp;
-        int offset;
         if(all_numbers(arguments.to_t)) {
             timestamp = atoll(arguments.to_t);
         } else {
-            parse_date_basic(arguments.to_t, &timestamp, &offset);
+            timestamp = approxidate(arguments.to_t);
         }
         json_object_set_new(match_cond, "to_t", json_integer(timestamp));
     }
     if(arguments.from_tm) {
         timestamp_t timestamp;
-        int offset;
         if(all_numbers(arguments.from_tm)) {
             timestamp = atoll(arguments.from_tm);
         } else {
-            parse_date_basic(arguments.from_tm, &timestamp, &offset);
+            timestamp = approxidate(arguments.from_tm);
         }
         json_object_set_new(match_cond, "from_tm", json_integer(timestamp));
     }
     if(arguments.to_tm) {
         timestamp_t timestamp;
-        int offset;
         if(all_numbers(arguments.to_tm)) {
             timestamp = atoll(arguments.to_tm);
         } else {
-            parse_date_basic(arguments.to_tm, &timestamp, &offset);
+            timestamp = approxidate(arguments.to_tm);
         }
         json_object_set_new(match_cond, "to_tm", json_integer(timestamp));
     }
